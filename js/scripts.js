866b8c17a78bb038887c7263d02920dc7719ce14
MrMendonez <dmendonez@gmail.com>
2015-11-18 21:01:32 -0500

    got new tasks along with working delete btn to appear on todo list. Now I need to include complete button.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,49 @@
 // Scripts for To Do List app
 
+$(document).ready(function() {
+  $(".alert-danger").hide();
+
+  $(".btn-warning").on("click", function(e) {
+    e.preventDefault();
+
+    var newWord = $("#newWord").val().trim();
+    var isDuplicate = false;
+    $("td.word-td").each(function(){
+      if($(this).text().trim().toLowerCase() === newWord.toLowerCase()) {
+        isDuplicate = true;
+        return;
+      }
+    });
+
+    if(isDuplicate) {
+      $(".alert-danger").slideDown();
+      return;
+    }
+
+    var newRow = $("<tr>");
+    var wordTd = $("<td>").addClass("word-td").append(newWord);
+    var deleteBtn = $("<button>").addClass("btn btn-danger").append('<i class="fa fa-times"></i>');
+    var deleteTd = $("<td>").append(deleteBtn);
+
+    newRow.append(wordTd).append(deleteTd);
+    $("tbody").append(newRow);
+
+    $("#newWord").val("").focus();
+  });
+
+  $("table").on("click", ".btn-danger", function() {
+    if($("tr").length > 2) {
+      $(this).parent().parent().remove();
+    }
+
+  });
+
+  $("#newWord").on("keydown", function() {
+    $(".alert-danger").fadeOut(1500);
+
+  })
+});
+
 // Variables
 var i;
 var remove = document.getElementsByClassName('remove-button');
@@ -21,7 +65,6 @@ function rowRemove() {
 for (i = 0; i < strikethrough.length; i++) {
   strikethrough[i].addEventListener('click', rowStrikethrough);
 };
-
 
 // Function to strikeout completed task
 function rowStrikethrough() {

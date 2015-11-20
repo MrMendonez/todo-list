147e0885f0df9d14e1eb3cc126f2002bd80243df
MrMendonez <dmendonez@gmail.com>
2015-11-19 22:58:32 -0500

    added comments to js file. changed x to trashcan icon. moved new task to right side.

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,41 @@
 // Scripts for To Do List app
 
 $(document).ready(function() {
+  // Hide duplicate message
   $(".alert-danger").hide();
 
   $(".btn-warning").on("click", function(e) {
-    e.preventDefault();
+    e.preventDefault();  // Prevent submit button from submitting text to the page
 
     var newWord = $("#newWord").val().trim();
     var isDuplicate = false;
+    // Finds duplicate entries
     $("td.word-td").each(function(){
       if($(this).text().trim().toLowerCase() === newWord.toLowerCase()) {
         isDuplicate = true;
         return;
       }
     });
-
+    // Runs duplicate entry message
     if(isDuplicate) {
       $(".alert-danger").slideDown(1000);
       return;
     }
-
+    // Inserts new row with new task item
     var newRow = $("<tr>");
     var wordTd = $("<td>").addClass("word-td").append(newWord);
     var completedBtn = $("<button>").addClass("btn btn-success strikethrough-button").append('<i class="fa fa-check">');
     var completedTd = $("<td>").append(completedBtn);
-    var deleteBtn = $("<button>").addClass("btn btn-danger").append('<i class="fa fa-times"></i>');
+    var deleteBtn = $("<button>").addClass("btn btn-danger").append('<i class="fa fa-trash-o"></i>');
     var deleteTd = $("<td>").append(deleteBtn);
 
     newRow.append(wordTd).append(completedTd).append(deleteTd);
-    $("tbody").append(newRow);
-
+    $("#task-list").append(newRow);
+    // Set newWord to become an empty string and clears the input field.
+    // Also sets the focus back onto the input field. 
     $("#newWord").val("").focus();
   });
-
-  $("table").on("click", ".btn-danger", function() {
-    if($("tr").length > 2) {
-      $(this).parent().parent().remove();
-    }
-
-  });
-
+  // Fade out duplicate entry message when user begins typing again in input field
   $("#newWord").on("keydown", function() {
     $(".alert-danger").fadeOut(1500);
 
